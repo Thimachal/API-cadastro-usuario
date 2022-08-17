4a621c6597cceb52e098b48743dfa1e43a6e36da
Thiago Oliveira <thimachal@gmail.com>
2022-08-16 21:27:16 -0300

    configuracao_porta_heroku

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ app.use(express.json());
 
 app.use(routes);
 
+app.listen(process.env.PORT || 3000);
+
+
+//abaixo a porta para servidor local, caso queira testar no seu computador
+/*
 app.listen(3000, () => {
     console.log('Server runnig on port http://localhost:3000');
 
 });
+*/
