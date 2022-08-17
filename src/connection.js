f78de0741e7ee1b581c6ff8658c830a2c9e9b96c
Thiago Oliveira <thimachal@gmail.com>
2022-08-16 21:43:32 -0300

    configuracao_pool

--- src/connection.js
+++ src/connection.js
@@ -1,4 +1,26 @@
-const knex = require('knex')({
+const { Pool } = require('pg');
+
+//configuração Heroku
+const pool = new Pool({
+    client: 'pg',
+    connection: {
+      host : 'ec2-44-206-197-71.compute-1.amazonaws.com',
+      database : 'dcoop5pcgtj8e8', //api-cad-users
+      user : 'zrfassfptqnnaa',      
+      password : '951f2df2b3c56a786b1c74054617396f12f00b6a58af12e30583a3825e8aeef4',
+      port : 5432,
+      ssl: {
+        rejectUnauthorized: false
+      }
+      
+    }
+  });
+  module.exports = knex;
+
+
+/*Conexão banco de dados local
+
+  const knex = require('knex')({
     client: 'pg',
     connection: {
       host : 'localhost',
@@ -9,3 +31,4 @@ const knex = require('knex')({
     }
   });
   module.exports = knex;
+  */
