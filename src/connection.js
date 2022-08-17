9b79d751223a19673150ff3fe23c19e1665bd9dc
Thiago Oliveira <thimachal@gmail.com>
2022-08-17 20:38:51 -0300

    json

--- src/connection.js
+++ src/connection.js
@@ -3,7 +3,7 @@ const knex = require('knex')({
   connection: {
     host : 'ec2-44-206-197-71.compute-1.amazonaws.com',
     user : 'zrfassfptqnnaa',
-    database : 'dcoop5pcgtj8e8', //api-cad-users
+    database : 'dcoop5pcgtj8e8',
     password : '951f2df2b3c56a786b1c74054617396f12f00b6a58af12e30583a3825e8aeef4',
     port : 5432,
     ssl: {
