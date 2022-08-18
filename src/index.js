2d38362eca5201ad77220b003760bb3e10377a28
Thiago Oliveira <thimachal@gmail.com>
2022-08-17 21:05:51 -0300

    json

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
-const cors = require('cors');
+
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3000);
